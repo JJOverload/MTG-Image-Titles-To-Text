bd2930e628598a1bdb885faf49ac0dca36546feb
JJOverload <153565873+JJOverload@users.noreply.github.com>
2024-02-05 11:45:21 -0800

    Update jsonparser.py
    - Sample code for accessing JSON data

--- jsonparser.py
+++ jsonparser.py
@@ -2,21 +2,39 @@
 # cd Documents\GitHub\MTG-Image-Titles-To-Text\
 
 import json
+
+
+with open('CardTypes.json', 'r', encoding="utf8") as CardTypes_file:
+    CardTypes_data = json.load(CardTypes_file)
+    #print(CardTypes_data)
+#print(json.dumps(CardTypes_data, indent=4))
+print(CardTypes_data["data"]["types"]["artifact"]["subTypes"])
+
+
+#Excerpt from JSON file
+"""
+"data": {
+        "types": {
+            "artifact": {
+                "subTypes": [
+                    "Attraction",
+                    "Blood",
+                    "Clue",
+                    "Contraption",
+                    "Equipment",
+                    ...
+"""
+
+'''
+with open('AtomicCards.json', 'r', encoding="utf8") as AtomicCards_file:
+    AtomicCards_data = json.load(AtomicCards_file)
+    #print(AtomicCards_data)
+print(json.dumps(AtomicCards_data, indent=4))
+'''
 '''
 with open('AllPrintings.json', 'r', encoding="utf8") as AllCards_file:
     AllCards_data = json.load(AllCards_file)
     #print(AllCards_data)
 print(json.dumps(AllCards_data, indent=4))
 '''
-'''
-with open('CardTypes.json', 'r', encoding="utf8") as AtomicCards_file:
-    AtomicCards_data = json.load(AtomicCards_file)
-    #print(AtomicCards_data)
-print(json.dumps(AtomicCards_data, indent=4))
-'''
-
-with open('AtomicCards.json', 'r', encoding="utf8") as AtomicCards_file:
-    AtomicCards_data = json.load(AtomicCards_file)
-    #print(AtomicCards_data)
-print(json.dumps(AtomicCards_data, indent=4))
 
