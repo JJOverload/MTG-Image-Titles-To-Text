03f2cea06bc4b01fc4037d3c38aaf51284e04f7d
JJOverload <153565873+JJOverload@users.noreply.github.com>
2024-02-09 22:21:55 -0800

    Update autocorrecter.py
    - Made modifications where we get each line to be the "word" to be autocorrected.

--- autocorrecter.py
+++ autocorrecter.py
@@ -9,22 +9,27 @@ import re
 from collections import Counter
 
 words = []
+'''
 with open('izzet-buylist.txt', 'r') as f:
     file_data = f.read()
     file_data = file_data.lower()
     words = re.findall("\\w+", file_data)
+'''
+with open('izzet-buylist.txt', 'r') as f:
+    words = f.readlines()
+
 V = set(words)
 print(f"The first sixteen words in the text are: \n{words[0:15]}")
 print(f"There are {len(V)} unique words in the vocabulary.")
 
 #Counter of word frequency
-print("Printing out counter of word frequency of 15 most common words.")
+print("-----Printing out counter of word frequency of 15 most common words.-----")
 word_freq_dict = {}
 word_freq_dict = Counter(words)
 print(word_freq_dict.most_common()[0:15])
 
 #Relative Frequency of words
-print("Printing out 15 most common words and their relative frequency within the whole file.")
+print("-----Printing out 15 most common words and their relative frequency within the whole file.-----")
 probs = {}
 Total = sum(word_freq_dict.values())
 for k in word_freq_dict.keys():
@@ -45,4 +50,6 @@ def mtg_autocorrect(input_word):
         output = df.sort_values(['Similarity', 'Prob'], ascending=False).head()
         return(output)
 
-print(mtg_autocorrect('arcane signet'))
+search_word = "arcane signet"
+print("-----Printing out results for " + search_word + "------")
+print(mtg_autocorrect(search_word))
