02442e73661990c8c0f867bbca8bfc4b5421cd8b
JJOverload <jay.szeto.23@gmail.com>
2024-02-16 20:20:18 -0800

    Update sample-tess-code.py + Create misc jpg files

--- tesseract-ocr/sample-tess-code.py
+++ tesseract-ocr/sample-tess-code.py
@@ -1,4 +1,6 @@
 #code found in: https://builtin.com/data-science/python-ocr
+# cd MTG-Image-Titles-To-Text/tesseract-ocr
+
 
 from PIL import Image
 import pytesseract
@@ -14,12 +16,25 @@ text = pytesseract.image_to_string(img1)
 
 print("First image text:", text)
 
-filename = '3_python-ocr.jpg'
+#filename = '3_python-ocr.jpg'
+filename = "CardPileSample1.jpg"
 img2 = np.array(Image.open(filename))
 text = pytesseract.image_to_string(img2)
 print("Second image text:", text)
 
 #-------------------------------
 
+#Using the same filename variable above
+img = np.array(Image.open(filename))
 #Cleaning up noisy image
-#norm_img = np.zeros((
+norm_img = np.zeros((img.shape[0], img.shape[1]))
+img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
+img = cv2.GaussianBlur(img, (1, 1), 0)
+cv2.imwrite("cleaned-output.jpg", img)
+text = pytesseract.image_to_string(img)
+print("Second image text (from cleaned):", text)
+
+
+
+
